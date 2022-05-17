7718546ba9390f301c8a6d3119377dc9d1b492f4
waflol <lenguyenhunganh2001@gmail.com>
2022-05-18 02:28:49 +0700

    update

--- utils.py
+++ utils.py
@@ -8,7 +8,8 @@ def get_data(path,seed=123,image_size=(512,512),batch_size=8, augmentation = Fal
     dataset = tf.keras.preprocessing.image_dataset_from_directory(path,seed=123,image_size=image_size,batch_size=batch_size)
     data_augmentation = tf.keras.Sequential([
         tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal"),
-        tf.keras.layers.experimental.preprocessing.RandomRotation(0.1)
+        tf.keras.layers.experimental.preprocessing.RandomFlip("vertical"),
+        tf.keras.layers.experimental.preprocessing.RandomRotation(0.5)
         ])
 
     if augmentation:
