1703f8980b1b102e898f82d2a1639b18b9f38e37
waflol <lenguyenhunganh2001@gmail.com>
2022-05-17 10:18:39 +0700

    Update

---
+++ train.py
@@ -0,0 +1,108 @@
+import warnings
+warnings.filterwarnings('ignore')
+
+import tensorflow as tf
+import numpy as np
+from keras.models import load_model
+from keras import Sequential,Model
+from keras.layers import Conv2D,MaxPooling2D,Flatten,Dense,Dropout,BatchNormalization,SeparableConv2D,Activation,GlobalAveragePooling2D,Add,Rescaling,Resizing
+from keras import backend as k
+from tensorflow.keras import applications
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import EarlyStopping,ModelCheckpoint, ReduceLROnPlateau,LearningRateScheduler,TensorBoard
+import datetime
+import tqdm
+import argparse
+import os,sys
+
+ROOT = ""
+def predictor(backbone,num_classes=1):
+    x = backbone.output
+    x = Flatten()(x)
+    x = Dense(1024,activation='relu')(x)
+    x = Dense(512,activation='relu')(x)
+
+    if num_classes == 2 or num_classes==1:
+        activation = "sigmoid"
+        units = 1
+    else:
+        activation = "softmax"
+        units = num_classes
+    x= Dropout(0.5)(x)
+    outputs = Dense(units,activation=activation)(x)
+    return Model(backbone.input,outputs)
+
+def train_on_epoch(x,y,model,loss_fn):
+    with tf.GradientTape() as tape:
+        logits = model(x, training=True)
+        loss_value = loss_fn(y, logits)
+    grads = tape.gradient(loss_value, model.trainable_weights)
+    return loss_value,logits,grads
+
+def val_on_epoch(x,y,model,loss_fn):
+    val_logits = model(x, training=False)
+    loss_value = loss_fn(y, val_logits)
+    val_logits = tf.argmax(val_logits, axis=1, output_type=tf.int32)
+    return val_logits, loss_value
+
+def train(path_hyp,backbone,augmentations=False,batch_size=4,imagesize=512):
+    pass
+
+def parse_opt(known=False):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', type=str, default=ROOT / 'yolov5s.pt', help='initial weights path')
+    parser.add_argument('--cfg', type=str, default='', help='model.yaml path')
+    parser.add_argument('--data', type=str, default=ROOT / 'data/coco128.yaml', help='dataset.yaml path')
+    parser.add_argument('--hyp', type=str, default=ROOT / 'data/hyps/hyp.scratch-low.yaml', help='hyperparameters path')
+    parser.add_argument('--epochs', type=int, default=300)
+    parser.add_argument('--batch-size', type=int, default=16, help='total batch size for all GPUs, -1 for autobatch')
+    parser.add_argument('--imgsz', '--img', '--img-size', type=int, default=640, help='train, val image size (pixels)')
+    parser.add_argument('--rect', action='store_true', help='rectangular training')
+    parser.add_argument('--resume', nargs='?', const=True, default=False, help='resume most recent training')
+    parser.add_argument('--nosave', action='store_true', help='only save final checkpoint')
+    parser.add_argument('--noval', action='store_true', help='only validate final epoch')
+    parser.add_argument('--noautoanchor', action='store_true', help='disable AutoAnchor')
+    parser.add_argument('--evolve', type=int, nargs='?', const=300, help='evolve hyperparameters for x generations')
+    parser.add_argument('--bucket', type=str, default='', help='gsutil bucket')
+    parser.add_argument('--cache', type=str, nargs='?', const='ram', help='--cache images in "ram" (default) or "disk"')
+    parser.add_argument('--image-weights', action='store_true', help='use weighted image selection for training')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--multi-scale', action='store_true', help='vary img-size +/- 50%%')
+    parser.add_argument('--single-cls', action='store_true', help='train multi-class data as single-class')
+    parser.add_argument('--optimizer', type=str, choices=['SGD', 'Adam', 'AdamW'], default='SGD', help='optimizer')
+    parser.add_argument('--sync-bn', action='store_true', help='use SyncBatchNorm, only available in DDP mode')
+    parser.add_argument('--workers', type=int, default=8, help='max dataloader workers (per RANK in DDP mode)')
+    parser.add_argument('--project', default=ROOT / 'runs/train', help='save to project/name')
+    parser.add_argument('--name', default='exp', help='save to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    parser.add_argument('--quad', action='store_true', help='quad dataloader')
+    parser.add_argument('--cos-lr', action='store_true', help='cosine LR scheduler')
+    parser.add_argument('--label-smoothing', type=float, default=0.0, help='Label smoothing epsilon')
+    parser.add_argument('--patience', type=int, default=100, help='EarlyStopping patience (epochs without improvement)')
+    parser.add_argument('--freeze', nargs='+', type=int, default=[0], help='Freeze layers: backbone=10, first3=0 1 2')
+    parser.add_argument('--save-period', type=int, default=-1, help='Save checkpoint every x epochs (disabled if < 1)')
+    parser.add_argument('--local_rank', type=int, default=-1, help='DDP parameter, do not modify')
+
+    # Weights & Biases arguments
+    parser.add_argument('--entity', default=None, help='W&B: Entity')
+    parser.add_argument('--upload_dataset', nargs='?', const=True, default=False, help='W&B: Upload data, "val" option')
+    parser.add_argument('--bbox_interval', type=int, default=-1, help='W&B: Set bounding-box image logging interval')
+    parser.add_argument('--artifact_alias', type=str, default='latest', help='W&B: Version of dataset artifact to use')
+
+    opt = parser.parse_known_args()[0] if known else parser.parse_args()
+    return opt
+
+def main(opt):
+    pass
+
+def run(**kwargs):
+    # Usage: import train; train.run(data='coco128.yaml', imgsz=320, weights='yolov5m.pt')
+    opt = parse_opt(True)
+    for k, v in kwargs.items():
+        setattr(opt, k, v)
+    main(opt)
+    return opt
+
+if __name__ == "__main__":
+    opt = parse_opt()
+    main(opt)
---
+++ utils.py
@@ -0,0 +1,28 @@
+import warnings
+warnings.filterwarnings('ignore')
+from keras.layers import Rescaling
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+def get_data(path,seed=123,image_size=(512,512),batch_size=8, augmentation = False):
+    dataset = tf.keras.preprocessing.image_dataset_from_directory(path,seed=123,image_size=image_size,batch_size=batch_size)
+    data_augmentation = tf.keras.Sequential([
+        tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal"),
+        tf.keras.layers.experimental.preprocessing.RandomRotation(0.1)
+        ])
+
+    if augmentation:
+        dataset = dataset.map(lambda x,y: (data_augmentation(x),y))
+    return dataset
+
+def show_sample(dataset,batch_size=8,num_row=1, class_name=None):
+    plt.figure(figsize=(10, 10))
+    for images, labels in dataset.take(1):
+        for i in range(batch_size):
+            ax = plt.subplot(num_row, batch_size//num_row, i + 1)
+            plt.imshow(images[i].numpy().astype("uint8"))
+            if class_name == None:
+                plt.title(str(labels[i]))
+            else:
+                plt.title(class_name[labels[i]])
+            plt.axis("off")
